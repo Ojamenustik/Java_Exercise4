1724c03adba87c267f013217cbc715b46532981c
Ojamenustik <ania.gajda96@gmail.com>
2020-04-02 17:41:28 +0200

    initializing and declaring vehicles and drivers arrays, creating personnel HashMap

--- src/com/ania/Main.java
+++ src/com/ania/Main.java
@@ -1,8 +1,22 @@
 package com.ania;
+import java.util.*;
 
 public class Main {
 
+    public static String[] vehicles = {
+            "ambulance", "helicopter", "lifeboat"
+    };
+
+    public static String[][] drivers = {
+            {"Fred", "Sue", "Pete"},
+            {"Sue", "Rich"},
+            {"Pete", "Rob"}
+    };
+
     public static void main(String[] args) {
-	// write your code here
+
+        Map<String, Set<String>> personnel = new HashMap<>();
+
+
     }
 }
