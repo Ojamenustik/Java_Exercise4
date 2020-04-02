488d1b3824a7136980e38a1df6c6a38728d9fd8b
Ojamenustik <ania.gajda96@gmail.com>
2020-04-02 17:57:28 +0200

    putting all vehicles as keys and their drivers in driverSet, brackets to scope, printing out whole thing

--- src/com/ania/Main.java
+++ src/com/ania/Main.java
@@ -17,6 +17,42 @@ public class Main {
 
         Map<String, Set<String>> personnel = new HashMap<>();
 
+        for(int i = 0; i < vehicles.length; i++){
+            String vehicle = vehicles[i];
+            String[] driversList = drivers[i];
+
+            Set<String> driverSet = new LinkedHashSet<String>();
+
+            for(String driver: driversList){
+                driverSet.add(driver);
+            }
+
+            personnel.put(vehicle, driverSet);
+        }
+
+        {///Brackets to scope driversList
+            //////Example usage
+            Set<String> driversList = personnel.get("helicopter");
+
+            for (String driver : driversList) {
+                System.out.println(driver);
+            }
+        }
+
+        /// Iterate through whole thing
+        for(String vehicle: personnel.keySet()){
+
+            System.out.print(vehicle);
+            System.out.print(": ");
+            Set<String> driversList = personnel.get(vehicle);
+
+            for(String driver: driversList){
+                System.out.print(driver);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
 
     }
 }
